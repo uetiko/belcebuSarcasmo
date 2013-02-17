a368e1b4b2a5287dc6e3f29a1443e59dd472ac80
Angel Barrientos <uetiko@gmail.com>
2013-02-16 18:29:06 -0600

    Alta de la herramienta Gii para crear la estructura de modulos y controladores

--- provocador/protected/config/main.php
+++ provocador/protected/config/main.php
@@ -7,7 +7,8 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'Activismo',
+        'defaultController'=>'index',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -23,14 +24,14 @@ return array(
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
+		
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'demo',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
-			'ipFilters'=>array('127.0.0.1','::1'),
+			//'ipFilters'=>array('127.0.0.1','::1'),
 		),
-		*/
+		/**/
 	),
 
 	// application components
