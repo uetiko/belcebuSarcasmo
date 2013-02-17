df0820c16c762c90746ab3acb812c4a7360d1b28
Angel Barrientos <uetiko@gmail.com>
2013-02-16 18:25:59 -0600

    Creando el esqueleto de modulos para usuarios y ong

---
+++ provocador/protected/modules/ong/controllers/RegistroController.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of RegistroController
+ *
+ * @author Angel Barrientos <uetiko@gmail.com>
+ */
+class RegistroController extends Controller{
+    //put your code here
+}
+
+?>
